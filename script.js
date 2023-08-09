d73cbb6dfdeb17756135fc1c05fa4a50dd7c80c3
Bruno Haddad <haddad.bruno00@gmail.com>
2023-08-09 11:33:45 -0300

    Commit inicial

---
+++ script.js
@@ -0,0 +1,26 @@
+
+function loginPage(){
+
+  window.location.href = "index.html"
+}
+
+function homePage(){
+
+  window.location.href = "home.html"
+}
+
+function goSimulator(){
+
+  window.location.href = "simulador.html"
+}
+
+function goProjection(){
+
+  window.location.href = "projecao.html"
+}
+
+function goResult(){
+
+  window.location.href = "result.html"
+}
+
