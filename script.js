b5f9438c5eb4a7ce8d81d31176039e6490c9bec5
Bruno Haddad <haddad.bruno00@gmail.com>
2023-08-09 12:37:51 -0300

    ajustes

--- script.js
+++ script.js
@@ -24,37 +24,20 @@ function goResult(){
   window.location.href = "result.html"
 }
 
-var message="";
-function clickIE() {if (document.all) {(message);return false;}}
-function clickNS(e) {if
-(document.layers||(document.getElementById&&!document.all)) {
-if (e.which==2||e.which==3) {(message);return false;}}}
-if (document.layers)
-{document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS;}
-else{document.onmouseup=clickNS;document.oncontextmenu=clickIE;}
-
-document.oncontextmenu=new Function("return false")
-
-
-//  F12
-//==========
-
-  document.onkeypress = function (event) {
-    if (e.ctrlKey &&
-        (e.keyCode === 123)) {
-            // alert('not allowed');
-            return false;
-          }
-  };
-
-
-//    CTRL + u
-//==============
-
-  document.onkeydown = function(e) {
-    if (e.ctrlKey &&
-      (e.keyCode === 85)) {
-          // alert('not allowed');
-          return false;
-        }
-      };  
+document.onkeydown = (e) => {
+  if (e.key == 123) {
+      e.preventDefault();
+  }
+  if (e.ctrlKey && e.shiftKey && e.key == 'I') {
+      e.preventDefault();
+  }
+  if (e.ctrlKey && e.shiftKey && e.key == 'C') {
+      e.preventDefault();
+  }
+  if (e.ctrlKey && e.shiftKey && e.key == 'J') {
+      e.preventDefault();
+  }
+  if (e.ctrlKey && e.key == 'U') {
+      e.preventDefault();
+  }
+};
